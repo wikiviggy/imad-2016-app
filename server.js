f0680f6b488a2b01f0bc460a88989e65fa81ff45
wikiviggy <vgnshprabhakar@gmail.com>
2016-09-22 08:53:56 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,6 +8,15 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+app.get('/article-one',function (req, res){
+    res.sendfile('article one requested and will be served');
+});
+app.get('/article-two',function (req, res){
+    res.sendfile('article two requested and will be served');
+});
+app.get('/article-three',function (req, res){
+    res.sendfile('article three requested and will be served');
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
