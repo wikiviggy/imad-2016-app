4905d7c0d6e964035cf6706b1687bff5853995fc
wikiviggy <vgnshprabhakar@gmail.com>
2016-09-25 18:49:50 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,8 +5,58 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+
+var articleone={
+    title:'article-1|wikiviggy',
+    heading:'article-one',
+    date:'septembre 22,2016',
+    content:`<p>
+            this is content of my first article. bonjour ! enchante . comment cava ?
+        </p>
+        <p>
+            je mapelle vignesh prabhakar. j ai 18 ans. j habite a' chennai au india . 
+        </p>
+        <p>j etudie ingenerie d' informatique en SRM universite'. j aime chocolats . j deteste' iitians. 
+        </p>
+        <p>a bientot . the nightmares are REAL !!! BALOR CLUB WORRLDWIDE !
+        </p>`
+};
+
+function createtemplate(data){
+var title=data.title;
+var date=data.date;
+var heading=data.heading;
+var content=data.content;
+
+var htmltempate=`<html>
+   <head>
+       <title>
+           ${title}
+       </title>
+   <meta name="viewport" content="width-device-width,initial-scale-1" \>
+   <link href="/ui/style.css" rel="stylesheet" />
+   </head>
+    <body>
+        <div class="koodigrah"> 
+        <div >
+        <a href="/">Home</a>
+    </div>
+    <hr/>
+    <h3>${heading}</h3>
+     <h3>correct the spelling errors</h3>    
+    <div>
+        ${date}
+    </div>
+    <div>
+      ${content}
+    </div>
+    </div>
+</body>
+</html>`;
+return htmltemplate;
+}
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+  res.send(createtemplate(articleone));
 });
 app.get('/article-one',function (req, res){
     res.sendfile(path.join(__dirname, 'ui', 'article-one.html'));
