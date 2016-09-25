14df2a1dbe5067e50435027ec01048cfc8effa92
wikiviggy <vgnshprabhakar@gmail.com>
2016-09-25 19:12:24 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-
-var articleone={
+var articles={
+'article-one':{
     title:'article-1|wikiviggy',
     heading:'article-one',
     date:'septembre 22,2016',
@@ -20,8 +20,38 @@ var articleone={
         </p>
         <p>a bientot . the nightmares are REAL !!! BALOR CLUB WORRLDWIDE !
         </p>`
+},
+'article-two':{
+        title:'article-2|wikiviggy',
+    heading:'article-one',
+    date:'septembre 23,2016',
+    content:`<p>
+            this is content of my second article. bonjour ! enchante . comment cava ?
+        </p>
+        <p>
+            je mapelle vignesh prabhakar. j ai 18 ans. j habite a' chennai au india . 
+        </p>
+        <p>j etudie ingenerie d' informatique en SRM universite'. j aime chocolats . j deteste' iitians. 
+        </p>
+        <p>a bientot . the nightmares are REAL !!! BALOR CLUB WORRLDWIDE !
+        </p>`
+},
+'article-three':{
+    title:'article-3|wikiviggy',
+    heading:'article-two',
+    date:'septembre 24,2016',
+    content:`<p>
+            this is content of my third article. bonjour ! enchante . comment cava ?
+        </p>
+        <p>
+            je mapelle vignesh prabhakar. j ai 18 ans. j habite a' chennai au india . 
+        </p>
+        <p>j etudie ingenerie d' informatique en SRM universite'. j aime chocolats . j deteste' iitians. 
+        </p>
+        <p>a bientot . the nightmares are REAL !!! BALOR CLUB WORRLDWIDE !
+        </p>`
+}
 };
-
 function createtemplate(data){
 var title=data.title;
 var date=data.date;
@@ -56,16 +86,11 @@ var htmltempate=`<html>
 return htmltemplate;
 }
 app.get('/', function (req, res) {
-  res.send(createtemplate(articleone));
+  res.sendfile(path.join(_dirname,'ui','index.html'));
 });
-app.get('/article-one',function (req, res){
-    res.sendfile(path.join(__dirname, 'ui', 'article-one.html'));
-});
-app.get('/article-two',function (req, res){
-    res.sendfile(path.join(__dirname, 'ui', 'article-two.html'));
-});
-app.get('/article-three',function (req, res){
-    res.sendfile(path.join(__dirname, 'ui', 'article-three.html'));
+app.get('/:articleName',function (req, res){
+   var articleName=req.params.articleName;
+    res.send(createtemplate(articles[articleName]));
 });
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
@@ -74,8 +99,6 @@ app.get('/ui/style.css', function (req, res) {
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
-
-
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
   console.log(`IMAD course app listening on port ${port}!`);
