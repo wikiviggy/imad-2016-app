1eb57d55ec5a58133930f288d24e43e19b49316f
wikiviggy <vgnshprabhakar@gmail.com>
2016-10-30 13:54:59 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -97,7 +97,7 @@ var pool = new Pool(config);
 app.get('/test-db',function(req,res){
     //make a select request
     //return a response with the results
-    pool.query('SELECT * FROM user',function(err,result){
+    pool.query('SELECT * FROM article', function(err,result){
         if(err){
             res.status(500).send(err.toString());
         }else{
