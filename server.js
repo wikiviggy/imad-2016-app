2a7bc6e5bbc1b5dece6ec66db490f86c9786ec9e
wikiviggy <vgnshprabhakar@gmail.com>
2016-10-31 19:36:14 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -118,6 +118,9 @@ app.get('/:articleName',function (req, res) {
    var articleName=req.params.articleName;
     res.send(createTemplate(articles[articleName]));
 });
+app.get('/ui/index.html', function(req, res){
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));   
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
