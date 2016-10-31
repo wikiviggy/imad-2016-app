9ebab74157621146953ac4f5205f93023d2794cb
wikiviggy <vgnshprabhakar@gmail.com>
2016-10-31 20:29:39 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -121,7 +121,7 @@ app.get('/:articleName',function (req, res) {
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
-app.get('/ui/madi.png', function( re,res) {
+app.get('/ui/madi.png', function( req,res) {
     res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 app.get('/ui/main.js', function (re,res) {
