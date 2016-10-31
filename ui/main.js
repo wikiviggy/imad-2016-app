de5bb70e440fa464139748274c4862b5280f3ef6
wikiviggy <vgnshprabhakar@gmail.com>
2016-10-31 20:31:06 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -8,7 +8,7 @@ var img=document.getElementById('madi');
 var marginleft=0;
 function moveright()
 {
-    marginleft=marginleft+10;
+    marginleft=marginleft+1;
     img.style.marginleft=marginleft+'px';
 }
 img.onclick=function(){
