554da6fd9434244fc15d2243f80a06221431e802
wikiviggy <vgnshprabhakar@gmail.com>
2016-11-07 19:17:51 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-var Pool= require('pg').pool;
+var Pool= require('pg').Pool;
 var app = express();
 var config={
     user:'wikiviggy',
