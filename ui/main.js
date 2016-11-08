07d2731b137770404199695c3d9c93afa499f2c2
wikiviggy <vgnshprabhakar@gmail.com>
2016-11-08 12:19:43 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,6 +2,7 @@ console.log('Loaded!');
 //change the text of main-text div
 var element=document.getElementById('main-text'
 );
+
 element.innerHtml= 'New value';
 //move the image
 var img=document.getElementById('wiki');
@@ -11,6 +12,6 @@ function moveright()
     marginleft=marginleft+1;
     img.style.marginleft=marginleft+'px';
 }
-img.onclick=function(){
+img.onclick=function() {
     var interval=setInterval(moveright,50);
 };
