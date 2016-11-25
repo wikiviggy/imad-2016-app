2bc7d616ac904947ca33e638334d686c8534425b
wikiviggy <vgnshprabhakar@gmail.com>
2016-11-25 16:45:28 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -244,6 +244,61 @@ var see=`<html>
 ;
 return see;
 }
+function stopwatch()
+{
+var me=`<html>
+<h1><time>00:00:00</time></h1>
+<button class="spbut"id="start">start</button>
+<button class="spbut"id="stop">stop</button>
+<button class="spbut"id="clear">clear</button>
+<script>
+var h1 = document.getElementsByTagName('h1')[0],
+start = document.getElementById('start'),
+stop = document.getElementById('stop'),
+clear = document.getElementById('clear'),
+seconds = 0, minutes = 0, hours = 0,
+t;
+
+function add() {
+ seconds++;
+if (seconds >= 60) {
+ seconds = 0;
+ minutes++;
+if (minutes >= 60) {
+ minutes = 0;
+hours++;
+ }
+ }
+
+ h1.textContent = (hours ? (hours > 9 ? hours : "0" + hours) : "00") + ":" + (minutes ? (minutes > 9 ? minutes : "0" + minutes) : "00") + ":" + (seconds > 9 ? seconds : "0" + seconds);
+
+timer();
+ }
+function timer() {
+t = setTimeout(add, 1000);
+}
+timer();
+
+
+/* Start button */
+start.onclick = timer;
+
+/* Stop button */
+stop.onclick = function() {
+clearTimeout(t);
+}
+
+/* Clear button */
+clear.onclick = function() {
+h1.textContent = "00:00:00";
+seconds = 0; minutes = 0; hours = 0;
+  }
+
+</script>
+</html> `
+;
+return me;
+}
 app.get('/', function (req, res) {
   res.sendfile(path.join(__dirname,'ui','index.html'));
 });
@@ -261,7 +316,9 @@ app.get('/my-login',function (req,res){
 app.get('/calculator', function (req,res) {
 res.send(calc());
 });
-
+app.get('/stop', function (req,res) {
+ res.send(stopwatch());   
+});
 app.get('/:articleName',function (req, res) {
    var articleName=req.params.articleName;
     res.send(createTemplate(articles[articleName]));
