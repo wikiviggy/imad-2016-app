cfdcc482c39a1afc96b95c34522671f5da17da47
wikiviggy <vgnshprabhakar@gmail.com>
2016-11-26 20:35:14 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -393,6 +393,9 @@ app.get('/ui/madi.png', function( req,res) {
 app.get('/ui/:fileName', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', req.params.fileName));
 });
+app.get('/login', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'login.html'));
+});
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
