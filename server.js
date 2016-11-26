8ec8aa0d0ac0b8bede4b6e84ec8d06a155b24f77
wikiviggy <vgnshprabhakar@gmail.com>
2016-11-26 20:29:45 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -124,53 +124,7 @@ var htmlTemplate=`<html>
 ;
 return htmlTemplate;
 }
-function login () {
 
-var page=`<html>
-<head>
-<style>
-       body
-              {
-                         background-image:url("https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcR2ChMQxpzikur_oqrbesow3tgAmWDD4f5Iq80ru3Idxz4Z_DgK");
-                                }
-                                       </style>
-
-<title>
-Login page
-</title>
-<meta name="viewport" content="width-device-width,initial-scale-1" \>
-</head>
-<body>
-<a class="evil"href="/">Home</a>
-<h1 style="font-family:Comic Sans Ms;text-align="center";font-size:20pt;
-color:#00FF00;>
-Simple Login Page to access and comment upon my third article 
-</h1>
-<form name="login">
-Username<input type="text" name="userid"/>
-Password<input type="password" name="pswrd"/>
-<input type="button" onclick="check(this.form)" value="Login"/>
-<input type="reset" value="Cancel"/>
-</form>
-<script language="javascript">
-function check(form)/*function to check userid & password*/
-{
- /*the following code checkes whether the entered userid and password are matching*/
-  if(form.userid.value == "myuserid" && form.pswrd.value == "mypswrd")
-    {
-        window.open('article-three')/*opens the target page while Id & password matches*/
-          }
-           else
-            {
-               alert("Error Password or Username")/*displays error message*/
-                 }
-                 }
-                 </script>
-                 </body>
-                 </html> `
-                 ;
-                 return page;
-}
 function hash (input, salt) {
     // How do we create a hash?
     var hashed = crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
@@ -416,9 +370,7 @@ app.get('/counter',function (req,res){
 app.get('/mymap', function  (req,res) {
     res.send(place());
 });
-app.get('/my-login',function (req,res){
-  res.send(login());
-});
+
 app.get('/calculator', function (req,res) {
 res.send(calc());
 });
